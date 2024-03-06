704fd718e5cfd5392360e505940343293ea96d71
Adil Zamal <adil.zamal@juspay.in>
2024-03-06 11:10:32 +0530

    hotel details form

---
+++ frontend/src/froms/ManageHotelForm/ManageHotelForm.jsx
@@ -0,0 +1,15 @@
+import { FormProvider, useForm } from 'react-hook-form'
+import DetailsSection from './DetailsSection'
+
+const ManageHotelForm = () => {
+    const formMethods = useForm()
+    return (
+        <FormProvider {...formMethods}>
+            <form>
+                <DetailsSection/>
+            </form>
+        </FormProvider>
+    )
+}
+
+export default ManageHotelForm;
---
+++ frontend/src/pages/AddHotel.jsx
@@ -0,0 +1,6 @@
+import ManageHotelForm from "../froms/ManageHotelForm/ManageHotelForm"
+
+const AddHotel = () => {
+    return (<ManageHotelForm />)
+}
+export default AddHotel;
